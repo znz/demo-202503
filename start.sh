6eac8290b128c96d1b1d6271def2b403e018db5d
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2025-03-18 19:08:53 +0900

    Add ingress for argocd

--- start.sh
+++ start.sh
@@ -91,3 +91,26 @@ for node in $(kind get nodes -n argocd); do
 [host."http://${reg_name}:${reg_port}"]
 EOF
 done
+
+# 5. Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "${reg_name}:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+
+kubectl apply -f registry/namespace.yaml
+kubectl apply -f registry
+
+# docker pull ubuntu
+# docker tag ubuntu registry.localhost:20080/ubuntu
+# docker push registry.localhost:20080/ubuntu
+# kubectl describe pod/ubuntu
+# kubectl delete pod/ubuntu
