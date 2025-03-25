13868a27437f88df8d74d2387f05b88763c38f75
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2025-03-25 13:58:52 +0900

    Remove git-server

--- cleanup.sh
+++ cleanup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
-D=$(dirname "$0")
-
-## git-server
-{
-    pushd "$D/git-server"
-    docker compose down --remove-orphans --rmi local --volumes
-    popd
-}
 
 ## kind
 kind delete cluster --name argocd
